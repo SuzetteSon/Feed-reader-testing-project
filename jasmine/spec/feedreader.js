0947cfaa2be73dc221e7c75147c440121683c414
Suzette Sonnekus <suzetsonekis@gmail.com>
2018-07-03 11:54:19 +0200

    Testing of clicks on menu successfull

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -62,30 +62,63 @@ $(function() {
          * hiding/showing of the menu element.
          */
 
-        it('Menu element is hidden by default', function() {
-            const body = document.querySelector('body');
+        it('menu element is hidden by default', function() {
+            let body = document.querySelector('body');
             expect(body.className).toBe('menu-hidden');
-        })
+        });
 
-
-    })
-
-        
-
-         /* TODO: Write a test that ensures the menu changes
+        /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
 
+        it('menu displays when clicked and clicked again', function() {
+            let event = $('.menu-icon-link');
+            let body = document.querySelector('body');
+            event.click();
+            expect(body.className).not.toBe('menu-hidden');
+
+            event.click();
+            expect(body.className).toBe('menu-hidden');
+        });
+
+    })
+
+        
+
+         
+
     /* TODO: Write a new test suite named "Initial Entries" */
 
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+
+        beforeEach(function(done) {
+
+
+            done();
+
+        });
+
+        it('loadFeed function has at least a single .entry element within the .feed container', function(done) {
+
+                done();
+
+        
+        });
+
+        
+
+    });
+
+
+        
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
